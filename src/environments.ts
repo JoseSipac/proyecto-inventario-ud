5259bc0d5c62d1e658dd0b37839d80875adeb370
Jose Sipac <geovany.compu@gmail.com>
2026-02-08 22:19:41 -0600

    chore: cambio minimo para websocket

--- src/environments.ts
+++ src/environments.ts
@@ -1,6 +1,6 @@
 // src/environments.ts
 export const environment = {
   production: false,
-  // API local (cuando usas ng serve) siempre es asi 
+  // API local (cuando usas ng serve) siempre es asi cambio 1 
   apiBaseUrl: 'http://localhost:3000'
 };
